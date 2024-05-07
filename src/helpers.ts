3661d7b1fa5eda8976fabebf7c9c56e939df3f83
Valentine <vpanchin@gmail.com>
2024-05-07 20:07:02 +0400

    Removing files

--- src/helpers.ts
+++
@@ -1,9 +0,0 @@
-import { ErrorMessage } from './types';
-
-export function isErrorWithMessage(
-  error: unknown,
-): error is ErrorMessage {
-  return (
-    typeof error === 'object' && error != null && 'data' in error
-  )
-}
